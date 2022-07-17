ef0e3ba1d28bb739c09b1c2406605252d5ec5a14
bpfox77 <56087137+bpfox77@users.noreply.github.com>
2022-07-17 17:05:23 -0500

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,139 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { useState, useEffect } from 'react';
+import { NftGallery } from 'react-nft-gallery';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import { providers } from 'ethers';
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Input,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import { debounce } from 'lodash';
+
+const Home: NextPage = () => {
+  const [wcProvider, setWcProvider] = useState<WalletConnectProvider>();
+  const [web3Provider, setWeb3Provider] = useState<providers.Web3Provider>();
+  const [walletAddr, setWalletAddr] = useState<string>();
+
+  const connectWallet = async () => {
+    try {
+      //  Enable session (triggers QR Code modal)
+      const wc = new WalletConnectProvider({
+        infuraId: '0fb70b3861104a29bb2f497a45cb34eb',
+      });
+      setWcProvider(wc);
+      setWeb3Provider(new providers.Web3Provider(wc));
+      await wc.enable();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const disconnectWallet = async () => {
+    await wcProvider?.disconnect();
+  };
+
+  const onReset = () => {
+    setWcProvider(undefined);
+    setWeb3Provider(undefined);
+    setWalletAddr(undefined);
+  };
+
+  useEffect(() => {
+    const getWalletAddr = async () => {
+      const accounts = await web3Provider?.listAccounts();
+      console.log(accounts);
+
+      if (accounts?.[0] && accounts[0] !== walletAddr) {
+        setWalletAddr(accounts[0]);
+      }
+    };
+
+    if (web3Provider) {
+      // web3Provider.on('accountsChanged', (accounts: string[]) => {
+      //   console.log('accountsChanged: ', accounts);
+      // });
+      getWalletAddr();
+    }
+  }, [web3Provider, walletAddr]);
+
+  const hasWalletAddr = typeof walletAddr === 'string';
+
+  return (
+    <div>
+      <Head>
+        <title>react-nft-gallery</title>
+        <meta
+          name="description"
+          content="Demo application for react-nft-gallery"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Heading as="h1" textAlign="center">
+          🖼 ✨ react-nft-gallery
+        </Heading>
+        {hasWalletAddr && (
+          <VStack alignItems="flex-end" p="8">
+            <Button variant="outline">
+              Wallet: {walletAddr!.length ? walletAddr : '<none>'}
+            </Button>
+            <Button onClick={onReset}>Change Wallet</Button>
+            {web3Provider && (
+              <Button
+                onClick={() => {
+                  disconnectWallet();
+                  onReset();
+                }}
+              >
+                Disconnect Wallet
+              </Button>
+            )}
+          </VStack>
+        )}
+
+        {hasWalletAddr ? (
+          <NftGallery ownerAddress={walletAddr!} darkMode />
+        ) : (
+          <VStack
+            height="100vh"
+            margin="auto"
+            justifyContent="center"
+            alignItems="center"
+            spacing="6"
+            px="6"
+          >
+            <Button onClick={connectWallet}>
+              <Image
+                src="/walletconnect-logo.svg"
+                alt="WalletConnect Logo"
+                height="4"
+                pr="2"
+              />
+              Connect Wallet
+            </Button>
+            <span>OR</span>
+            <Input
+              maxWidth={{ base: '100%', md: '50%', lg: '33%' }}
+              placeholder="ENS domain (subek.eth) or 0x..."
+              onChange={debounce(
+                (evt) => setWalletAddr(evt.target.value),
+                1000
+              )}
+            />
+            <span>OR</span>
+            <Button onClick={() => setWalletAddr('')}>See Latest Mints</Button>
+          </VStack>
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
